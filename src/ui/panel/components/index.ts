a94e346edca3a2fbd838e210dc8beb72efb3a414
letsjo <hyunoh.jo@gmail.com>
2026-02-22 23:14:46 +0900

    refactor: remove obsolete ui panel barrel files

--- src/ui/panel/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { PanelHeader } from "./PanelHeader";
-export { PanelWorkspace } from "./PanelWorkspace";
-export { PanelFooter } from "./PanelFooter";
-export { WorkspaceCanvas } from "./WorkspaceCanvas";
-export { WorkspacePanels } from "./WorkspacePanels";
-export { WorkspacePanel } from "./WorkspacePanel";
-export * from "./panels";
